8b4c4bdd3a3cd4acec787afb554c804d019a4bf3
舒杰 <1499570221@qq.com>
2021-09-09 13:06:35 +0000

    rename neon/neon_sin.c to neon/neon_instr.c.

--- neon/neon_instr.c
+++ neon/neon_instr.c
@@ -1,5 +1,5 @@
 /*
- * neon_sin.c
+ * neon_instr.c
  * 
  * Copyright 2021 Ubuntu-64-Develop <sn02241@ubuntu>
  * 
@@ -23,68 +23,26 @@
 
 
 #include <stdio.h>
-#include <arm_neon.h>
 #include <sys/time.h>
+#include <stdlib.h>
+#include <arm_neon.h>
 
-#define EPSILON     1e-6
+#define PIXEL      256
+#define RGB_SIZE   1024
 
-
-int cSin(float x)
-{
-    int i;
-    int s;
-    float sum = 0;
-    float mole;   //分子
-    float deno;   // 分母
-    
-    mole = x;
-    deno = 1;
-    s = 1;
-    for(i = 1; (mole / deno) >= EPSILON; i++)
-    {
-        sum  = sum + s * (mole / deno);
-        mole = mole * x * x;
-        deno = deno * 2 * i * (2 * i + 1);
-        s *= -1;
-    }
-    
-    printf("sin(%f) ≈ %f\n", x, sum);
-    return 0;
-}
-
-
-int neonSin(float x)
-{
-        int i;
-    int s;
-    float sum = 0;
-    float mole;   //分子
-    float deno;   // 分母
-    
-    mole = x;
-    deno = 1;
-    s = 1;
-    kernel_neon_begin();
-    for(i = 1; (mole / deno) >= EPSILON; i++)
-    {
-        sum  = sum + s * (mole / deno);
-        mole = mole * x * x;
-        deno = deno * 2 * i * (2 * i + 1);
-        s *= -1;
-    }
-    kernel_neon_end();
-    
-    printf("sin(%f) ≈ %f\n", x, sum);
-}
-
+void rgb_c(uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *rgb, int lenColor);
+void rgb_neon(uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *rgb, int lenColor);
 
 int main(int argc, char **argv)
 {
-	float data;
+	uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t rgb;
     struct timeval startTime;
     struct timeval endTime;
     double timeUsed;
-    scanf("x waiting to calc: %f", &data);
+    
     gettimeofday(&startTime, NULL);
     cSin(data);
     gettimeofday(&endTime, NULL);
@@ -103,4 +61,38 @@ int main(int argc, char **argv)
 
 
 
+void rgb_c(uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *rgb, int lenColor)
+{
+    for(i = 0;i < lenColor; i++)
+    {
+        r[i] = rgb[3 * i];
+        g[i] = rgb[3 * i + 1];
+        b[i] = rgb[3 * i + 2];
+    }
+}
 
+void rgb_neon(uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *rgb, int lenColor)
+{
+    int num8x16 = lenColor / 16;
+    uint8x16x3_t intlv_rgb;
+    for (int i=0; i < num8x16; i++) 
+    {
+        intlv_rgb = vld3q_u8(rgb+3*16*i);   //将8位数据每16个一个寄存器，放到连续的3个寄存器
+        vst1q_u8(r+16*i, intlv_rgb.val[0]); //将一维向量写入CPU内存中
+        vst1q_u8(g+16*i, intlv_rgb.val[1]);
+        vst1q_u8(b+16*i, intlv_rgb.val[2]);
+    }
+}
+
+int random_num(uint8_t n, uint8_t *arr, int count)
+{
+    int i;
+    srand((unsigned)time(NULL));
+    for(i = 0; i < count; i++)
+    {
+        uint8_t rand = rand() % n;
+        rgb[i] = rand;
+    }
+    
+    return 0;
+}

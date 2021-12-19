bf3d7f474981b9aeac1ecc8cbb8cad54de98ccf5
舒杰 <9504858+shu-jie@user.noreply.gitee.com>
2021-12-20 00:04:33 +0800

    redesign the UI

--- pythonProject/UI/开发文件/MenuFrm.py
+++ pythonProject/UI/开发文件/MenuFrm.py
@@ -33,6 +33,40 @@ class MenuFrame(object):
         self.root = master
         self.createFrame()
 
+        """
+        参数初始化
+        """
+        self.frm = tk.Frame()
+        self.frm_status_top_left = tk.Frame()
+        self.frm_status_top_right = tk.Frame()
+
+        self.frm_status = tk.LabelFrame()
+        self.frm_status_top = tk.LabelFrame()
+        self.frm_status_bottom = tk.LabelFrame()
+        self.frm_function = tk.LabelFrame()
+        self.frm_function_top = tk.LabelFrame()
+        self.frm_func_top_copy = tk.LabelFrame()
+        self.frm_func_top_check = tk.LabelFrame()
+        self.frm_func_top_sdk = tk.LabelFrame()
+        self.frm_function_bot = tk.LabelFrame()
+
+        self.title_label = tk.Label()
+        self.windows_path_label = tk.Label()
+        self.linux_path_label = tk.Label()
+        self.connect_status_label = tk.Label()
+        self.connect_status = tk.Label()
+
+        self.ip_entry = tk.Entry()
+        self.user_entry = tk.Entry()
+        self.pd_entry = tk.Entry()
+        self.windows_path_entry = tk.Entry()
+        self.linux_path_entry = tk.Entry()
+
+        self.windows_path_var = tk.StringVar()
+
+        self.choose_path_btn = tk.Button()
+        self.copy_btn = tk.Button()
+
     def createFrame(self):
         """
         创建窗体,分为左右两部分,左边为状态栏,右边为功能区
@@ -48,6 +82,7 @@ class MenuFrame(object):
         self.frm_function.pack(fill='both', expand=1, side=tk.RIGHT)
 
         self.createStatusFrame()
+        self.createFunctionFrame()
 
     def createStatusFrame(self):
         """
@@ -94,14 +129,14 @@ class MenuFrame(object):
         :return: 无
         """
         self.windows_path_var = tk.StringVar()
-        self.frm_windows_path_temp = tk.Frame(self.frm_status_bottom)
-        self.windows_path_label = tk.Label(self.frm_windows_path_temp, text='Windows Path:', font=G_FONT)
-        self.choose_path_btn = tk.Button(self.frm_windows_path_temp, text='...', command=self.openDir)
+        frm_windows_path_temp = tk.Frame(self.frm_status_bottom)
+        self.windows_path_label = tk.Label(frm_windows_path_temp, text='Windows Path:', font=G_FONT)
+        self.choose_path_btn = tk.Button(frm_windows_path_temp, text='...', command=self.openDir)
         self.windows_path_entry = tk.Entry(self.frm_status_bottom, width=20, textvariable=self.windows_path_var)
         self.linux_path_label = tk.Label(self.frm_status_bottom, text='Linux Path:', font=G_FONT)
         self.linux_path_entry = tk.Entry(self.frm_status_bottom, width=20)
 
-        self.frm_windows_path_temp.pack(fill='both', expand=0, padx=5, pady=5)
+        frm_windows_path_temp.pack(fill='both', expand=0, padx=5, pady=5)
         self.windows_path_label.pack(fill='both', expand=0, padx=5, pady=5, side=tk.LEFT)
         self.choose_path_btn.pack(fill='x', expand=0, padx=5, pady=5, side=tk.RIGHT)
         self.windows_path_entry.pack(fill='both', expand=1, padx=5, pady=5)
@@ -110,37 +145,56 @@ class MenuFrame(object):
 
     def createFunctionFrame(self):
         """
-        功能区,主要分为三部分
+        功能区,主要分为两部分,上半部为功能选项区,下半区为打印区
         :return:
         """
 
         self.frm_function_top = tk.LabelFrame(self.frm_function)
-        self.frm_function_mid = tk.LabelFrame(self.frm_function)
         self.frm_function_bot = tk.LabelFrame(self.frm_function)
 
         self.frm_function_top.pack(fill='both', expand=1, padx=5, pady=5)
-        self.frm_function_mid.pack(fill='both', expand=1, padx=5, pady=5)
         self.frm_function_bot.pack(fill='both', expand=1, padx=5, pady=5)
 
         self.createFuncTopFrame()
-        self.createFuncMidFrame()
         self.createFuncBotFrame()
 
     def createFuncTopFrame(self):
         """
-        创建功能区的复制功能，分为上下两部分
+        创建功能选项区, 分为三部分
         :return:
         """
-        self.frm_func_top_top = tk.LabelFrame(self.frm_function_top)
-        self.frm_func_top_bot = tk.LabelFrame(self.frm_function_top)
+        self.frm_func_top_copy = tk.LabelFrame(self.frm_function_top)
+        self.frm_func_top_check = tk.LabelFrame(self.frm_function_top)
+        self.frm_func_top_sdk = tk.LabelFrame(self.frm_function_top)
 
-        self.frm_func_top_top.pack(fill='both', expand=1, padx=5, pady=5)
-        self.frm_func_top_bot.pack(fill='both', expand=1, padx=5, pady=5)
+        self.frm_func_top_copy.pack(fill='both', expand=0, padx=1)
+        self.frm_func_top_check.pack(fill='both', expand=0, padx=1)
+        self.frm_func_top_sdk.pack(fill='both', expand=0, padx=1)
 
-        self.connect_status_label = tk.Label(self.frm_func_top_top, text='status:', font=G_FONT)
-        self.connect_status = tk.Label(self.frm_func_top_top, text='', font=G_FONT)
+        self.createCopyFuncFrame()
+        self.createCheckFuncFrame()
+        self.createSDKFuncFrame()
 
-    def createFuncMidFrame(self):
+    def createCopyFuncFrame(self):
+        """
+        创建copy功能区
+        :return:
+        """
+        label_font = ('Monaco', 10)
+        self.connect_status_label = tk.Label(self.frm_func_top_copy, text='status:', font=G_FONT)
+        self.connect_status = tk.Label(self.frm_func_top_copy, text='disconnected', fg='red', font=label_font)
+        self.copy_btn = tk.Button(self.frm_func_top_copy, text='copy',
+                                  width=8, bg='lightblue', font=G_FONT,
+                                  command=self.copyClick)
+
+        self.connect_status_label.pack(fill='both', expand=0, padx=5, pady=5, side=tk.LEFT)
+        self.connect_status.pack(fill='both', expand=0, padx=5, pady=5, side=tk.LEFT)
+        self.copy_btn.pack(fill='both', expand=0, padx=5, pady=5, side=tk.RIGHT)
+
+    def createCheckFuncFrame(self):
+        pass
+
+    def createSDKFuncFrame(self):
         pass
 
     def createFuncBotFrame(self):
@@ -152,6 +206,9 @@ class MenuFrame(object):
         :return: 无
         """
         self.windows_path_var.set(fd.askdirectory())
+
+    def copyClick(self):
+        pass
 
 
 if __name__ == '__main__':

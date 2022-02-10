7d3ff12f82472e949cce78d3efbb20c95bd826c7
Shu Jie <shujie@rigol.com>
2022-02-10 18:26:33 +0800

    update
    Change-Id: I10d3365eb3f27c5fc9640c9c8ca8a451f8740d00

--- fpga_to_sdio/fpga_to_sdio.c
+++ fpga_to_sdio/fpga_to_sdio.c
@@ -12,8 +12,44 @@
 
 #include <mach/dma.h>
 
+#include "fpga_to_sdio.h"
+
 #define DEVICE_NAME   "fpga_to_sdio"
 
+/**
+ * 这是模仿host的主机端
+ * 目前代码不完整,缺失很大,并且流程不明确
+ */
+
+
+struct fpga_host {
+	struct platform_device *pdev;
+	struct fpga_to_sdio_pdata *pdata;
+	struct mmc_host		*mmc;
+	struct resource		*mem;
+	struct clk		*clk;         //当前的总线时钟
+	u8 pwr;                       //当前电压
+	
+	void __iomem   *base;
+	
+	
+	spinlock_t lock;
+	int        irq;         //中断  
+	int        irq_cd;
+	
+	int        dma;
+	int        do_dma;
+	int        dma_togo;
+	int        dma_complete;
+	
+	
+	struct mmc_request    *mrq;             //当前request
+	struct mmc_command    *cmd;             //当前命令
+	
+	int 	   isfpga;
+	
+	struct tasklet_struct	pio_tasklet;
+};
 
 static int fpga_remove(struct platform_device *pdev)
 {
@@ -23,9 +59,29 @@ static int fpga_remove(struct platform_device *pdev)
 static int fpga_probe(struct platform_device *pdev)
 {
 	struct mmc_host *mmc;
+	struct fpga_host *host;
 	int ret;
+	int isfpga;
+	
+	isfpga = platform_get_device_id(pdev)->driver_data;
+	
+	mmc = mmc_alloc_host(sizeof(struct fpga_host), &pdev->dev);   //分配mmc_host结构体内存
+	if(!mmc) {
+		ret = -ENOMEM;
+		goto out;
+	}
+	
+	host = mmc_priv(mmc); //将mmc_host和fpga_host关联
+	host->mmc = mmc;
+	host->pdev = pdev;
+	host->isfpga = isfpga;
+	
+	spin_lock_init(&host->lock);
 	
 	return 0;
+	
+out:
+	return ret;
 }
 
 static void fpga_shutdown(struct platform_device *pdev)
@@ -33,15 +89,48 @@ static void fpga_shutdown(struct platform_device *pdev)
 	
 }
 
+
+#ifdef CONFIG_PM
+
+static int fpga_suspend(struct device *dev)
+{
+	struct mmc_host *mmc = platform_get_drvdata(to_platform_device(dev));
+
+	return mmc_suspend_host(mmc);
+}
+
+static int fpga_resume(struct device *dev)
+{
+	struct mmc_host *mmc = platform_get_drvdata(to_platform_device(dev));
+
+	return mmc_resume_host(mmc);
+}
+
+static const struct dev_pm_ops fpga_pm = {
+	.suspend	= fpga_suspend,
+	.resume		= fpga_resume,
+};
+
+#define fpga_pm_ops &fpga_pm
+#else /* CONFIG_PM */
+#define fpga_pm_ops NULL
+#endif /* CONFIG_PM */
+
+
+
 static struct platform_device_id fpga_driver_ids[] = {
-	.name = "fpga_to_sdio",     //设备数里的匹配字段
+	.name = "fpga_to_sdio",     //匹配字段
+	.driver_data = 1,
 	{},
 }
+
+MODULE_DEVICE_TABLE(platform, fpga_driver_ids);
 
 static struct platform_driver fpga_driver = {
 	.driver = {
 		.name  = "fpga_to_sdio",
 		.owner = THIS_MODULE,
+		.pm    = fpga_pm_ops,
 	}
 	.id_table = fpga_driver_ids,
 	.probe    = fpga_probe,
---
+++ fpga_to_sdio/fpga_to_sdio.h
@@ -0,0 +1,14 @@
+#ifndef __FPGA_TO_SDIO_H__
+#define __FPGA_TO_SDIO_H__
+
+struct fpga_to_sdio_pdata {
+	unsigned int	max_width;   //可支持的最大传输bit
+	
+	
+	/**
+	 * 一些操作函数
+	 */
+};
+
+
+#endif
